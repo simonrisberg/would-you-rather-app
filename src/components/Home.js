565bee91d52eb6dba82e9c81c5a89cb4beaeed96
Simon Risberg <sirsimon40@gmail.com>
2021-11-07 16:57:33 +0100

    Added dispatch functionality to unanswered and answered buttons to be able to change the state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux'
+import { toggleUnAnsweredQuestions } from "../actions/unAnsweredQuestionsIsActive";
 import Poll from "./Poll";
 
 class Home extends Component {
+
+  showUnansweredQuestions = () => {
+    return () => {
+      this.props.dispatch(toggleUnAnsweredQuestions(true))
+    }
+  }
+
+  showAnsweredQuestions = () => {
+    return () => {
+      this.props.dispatch(toggleUnAnsweredQuestions(false))
+    }
+  }
 
   render() {
 
     return (
       <div className='home-container'>
         <div className='home-buttons'>
-          <button className='home-button'>Unanswered questions</button>
-          <button className='home-button'>Answered questions</button>
+          <button className='home-button' onClick={this.showUnansweredQuestions()} >Unanswered questions</button>
+          <button className='home-button' onClick={this.showAnsweredQuestions()}>Answered questions</button>
         </div>
         {this.props.questions.map((question) => (
           <li key={question.id}>
