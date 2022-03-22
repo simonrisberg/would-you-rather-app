df5e4f7efb8ade265b211089a8fd8dda1fe64964
Simon Risberg <simon@evello.org>
2022-03-22 16:45:47 +0100

    Added live data to leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -5,28 +5,36 @@ class LeaderBoard extends Component {
 
   render() {
 
+    const { users } = this.props
+
+    console.log("USERS", users)
+
     return (
+      
       <div className="leaderboard-container">
-        <div className='leaderboard'>
-          <img src={"https://tylermcginnis.com/would-you-rather/tyler.jpg"} alt={"Avatar"} className='leaderboard-img-container'></img>
+        {users.map((user) => (
+          <div className='leaderboard'>
+          <img src={user.avatarURL} alt={"Avatar"} className='leaderboard-img-container'></img>
           <div className='leaderboard-text-container'>
-            <h2>Simon Risberg</h2>
+            <h2>{user.name}</h2>
             <div className='leaderboard-questions-container'>
               <p>Answered questions</p>
-              <p>7</p>
+              <p>{Object.values(user.answers).length}</p>
             </div>
             <div className='leaderboard-questions-container'>
               <p>Created questions</p>
-              <p>3</p>
+              <p>{user.questions.length}</p>
             </div>
           </div>
           <div className='leaderboard-score-container'>
             <p className='leaderboard-score-text'>Score</p>
             <div className='leaderboard-score'>
-              <p style={{textAlign: "center", marginTop: "22px"}}>10</p>
+              <p style={{textAlign: "center", marginTop: "22px"}}>{user.questions.length + Object.values(user.answers).length}</p>
             </div>
           </div>
         </div>
+        ))}
+        
 
       </div>
     )
@@ -34,4 +42,10 @@ class LeaderBoard extends Component {
 
 }
 
-export default connect()(LeaderBoard)
+function mapStateToProps({ users }) {
+  return {
+    users: Object.values(users)
+  }
+}
+
+export default connect(mapStateToProps)(LeaderBoard)
