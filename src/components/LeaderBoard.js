9a436e144fa80e79624b676137509b52be966885
Simon Risberg <simon@evello.org>
2022-03-27 13:26:14 +0200

    Added functionality for ordering users in descdending order based on their total score

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -10,32 +10,32 @@ class LeaderBoard extends Component {
     console.log("USERS", users)
 
     return (
-      
+
       <div className="leaderboard-container">
         {users.map((user) => (
-          <div className='leaderboard'>
-          <img src={user.avatarURL} alt={"Avatar"} className='leaderboard-img-container'></img>
-          <div className='leaderboard-text-container'>
-            <h2>{user.name}</h2>
-            <div className='leaderboard-questions-container'>
-              <p>Answered questions</p>
-              <p>{Object.values(user.answers).length}</p>
+          <li key={user.id}>
+            <div className='leaderboard'>
+              <img src={user.avatarURL} alt={"Avatar"} className='leaderboard-img-container'></img>
+              <div className='leaderboard-text-container'>
+                <h2>{user.name}</h2>
+                <div className='leaderboard-questions-container'>
+                  <p>Answered questions</p>
+                  <p>{Object.values(user.answers).length}</p>
+                </div>
+                <div className='leaderboard-questions-container'>
+                  <p>Created questions</p>
+                  <p>{user.questions.length}</p>
+                </div>
+              </div>
+              <div className='leaderboard-score-container'>
+                <p className='leaderboard-score-text'>Score</p>
+                <div className='leaderboard-score'>
+                  <p style={{ textAlign: "center", marginTop: "22px" }}>{user.questions.length + Object.values(user.answers).length}</p>
+                </div>
+              </div>
             </div>
-            <div className='leaderboard-questions-container'>
-              <p>Created questions</p>
-              <p>{user.questions.length}</p>
-            </div>
-          </div>
-          <div className='leaderboard-score-container'>
-            <p className='leaderboard-score-text'>Score</p>
-            <div className='leaderboard-score'>
-              <p style={{textAlign: "center", marginTop: "22px"}}>{user.questions.length + Object.values(user.answers).length}</p>
-            </div>
-          </div>
-        </div>
+          </li>
         ))}
-        
-
       </div>
     )
   }
@@ -43,8 +43,15 @@ class LeaderBoard extends Component {
 }
 
 function mapStateToProps({ users }) {
+
+
+
+  const formattedUsers = Object.values(users).map(user => (
+    { ...user, totalScore: user.questions.length + Object.values(user.answers).length }
+  ))
+
   return {
-    users: Object.values(users)
+    users: formattedUsers.sort((a, b) => (a.totalScore > b.totalScore) ? -1 : 1)
   }
 }
 
